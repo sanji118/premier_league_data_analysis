5a810581e4b8fe2df04b6018958863eb2c93a350
sanji118 <sanjishifa32@gmail.com>
2026-09-10 12:00:22 +0600

    added highes scoring goals

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ print(data.head())
 data["TotalGoals"] = data["FTHG"]+data["FTAG"]
 
 print('\nTotal goals:', data["TotalGoals"].sum())
+print('Average goals per match:', round(data["TotalGoals"].mean(), 2))
+
+
+#highest scoring
+print("\nTop 10 highest-scoring matches:")
+top_matches = data.sort_values(by="TotalGoals", ascending=False)
+print(top_matches[["HomeTeam", "AwayTeam", "FTHG", "FTAG", "TotalGoals"]].head(10))
+
+#match results
+print(data["FTR"].value_counts())
+result = data["FTR"].value_counts()
