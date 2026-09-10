5c17bb588b5fecf05bdbc75e3dda96b95a674667
sanji118 <sanjishifa32@gmail.com>
2026-09-10 11:50:59 +0600

    checked 5 data from csv file

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+#Read data
+data = pd.read_csv('C:/programming_course_1/python-course/premier_league_data_analysis/premier_league_data.csv')
+print(data.head())
