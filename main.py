6717f23df21b62bf4d167668f7f418941c55fbcf
sanji118 <sanjishifa32@gmail.com>
2026-09-10 11:53:37 +0600

    checked total goals

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 #Read data
 data = pd.read_csv('C:/programming_course_1/python-course/premier_league_data_analysis/premier_league_data.csv')
 print(data.head())
+
+data["TotalGoals"] = data["FTHG"]+data["FTAG"]
+
+print('\nTotal goals:', data["TotalGoals"].sum())
